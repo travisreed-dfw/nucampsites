d63a9fd8feedca7686cc899387782662bc838c0c
spudworx <travisreed.dfw@gmail.com>
2020-11-28 12:22:00 -0600

    Changed modals to use jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function () {
+  // BEGIN CAROUSEL BUTTONS
   $('.carousel').carousel({ interval: 2000 });
   $('#carouselButton').click(function () {
     if ($('#carouselButton').children('i').hasClass('fa-pause')) {
@@ -11,4 +12,13 @@ $(function () {
       $('#carouselButton').children('i').addClass('fa-pause');
     }
   });
+  // END CAROUSEL BUTTONS
+  // BEGIN MODAL BUTTONS
+  $('#reserveButton').on('click', function () {
+    $('#reserveModal').modal('show');
+  });
+  $('#loginButton').on('click', function () {
+    $('#loginModal').modal('show');
+  });
+  // END MODAL BUTTONS
 });
